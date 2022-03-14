9e4ced2f5237b071930dfb5a9ad6db8216dc8f37
Abrorjon Ergashev <7abror7@gmail.com>
2022-03-14 13:48:07 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/b6cinemarestservice/B6CinemaRestServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.b6cinemarestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B6CinemaRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B6CinemaRestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/b6cinemarestservice/B6CinemaRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.b6cinemarestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B6CinemaRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
