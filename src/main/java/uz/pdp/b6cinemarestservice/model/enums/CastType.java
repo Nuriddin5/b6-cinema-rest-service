eb8f241566cc4de1ec58fff921b9be96c43dced5
Abrorjon Ergashev <7abror7@gmail.com>
2022-03-16 17:26:27 +0500

    modified Row, User, CastType

--- src/main/java/uz/pdp/b6cinemarestservice/model/enums/CastType.java
+++ src/main/java/uz/pdp/b6cinemarestservice/model/enums/CastType.java
@@ -1,6 +1,23 @@
 package uz.pdp.b6cinemarestservice.model.enums;
 
+import java.util.Arrays;
+
 public enum CastType {
-    CAST_ACTOR,
-    CAST_DIRECTOR
+    CAST_ACTOR("Aktyor"),
+    CAST_DIRECTOR("Rejisyor");
+
+
+    String displayName;
+
+    CastType(String displayName) {
+        this.displayName = displayName;
+    }
+
+
+    public static CastType getCastByDisplayName(String displayNameDto){
+
+        return Arrays.stream(
+                        CastType.values())
+                .filter(castType -> castType.displayName.equals(displayNameDto)).findFirst().get();
+    }
 }
